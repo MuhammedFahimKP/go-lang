d9bbf51c88a60297487a09f15d09465a5b59ea2b
muhammmedfahim4321 <fahimmuhammedfahimkp@gmail.com>
2024-01-09 14:53:35 +0530

    added go files

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Print("hai")
+
+	var conferenceName = "Go Reference"
+	const remainingTickets = 40
+
+	fmt.Printf("welcom to %#v", conferenceName)
+
+	fmt.Print(conferenceName)
+
+}
